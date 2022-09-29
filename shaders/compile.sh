d1d2b5ee8ee9ac755fcc85f694bd8ce8fe67885d
Mirkopoj <mirkopoj@hotmail.com>
2022-09-29 14:01:18 -0300

    Quedó en: Graphics pipeline basics -> Fixed functions

---
+++ shaders/compile.sh
@@ -0,0 +1,4 @@
+#!/bin/fish
+
+glslc /home/citecca/VulkanTutorial/shaders/shader.vert -o /home/citecca/VulkanTutorial/shaders/vert.spv
+glslc /home/citecca/VulkanTutorial/shaders/shader.frag -o /home/citecca/VulkanTutorial/shaders/frag.spv
